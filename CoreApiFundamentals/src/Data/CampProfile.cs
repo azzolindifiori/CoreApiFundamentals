278af0ea966705d06c436f1b0629b95d817bceff
Davide Azzolini <davide.azzolini@retailitalia.onmicrosoft.com>
2021-03-11 17:01:55 +0100

    Ended

--- CoreApiFundamentals/src/Data/CampProfile.cs
+++ CoreApiFundamentals/src/Data/CampProfile.cs
@@ -10,6 +10,14 @@ namespace CoreCodeCamp.Data
             CreateMap<Camp, CampModel>()
                 .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
                 .ReverseMap();
+
+            CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp , opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
         }
     }
 }
